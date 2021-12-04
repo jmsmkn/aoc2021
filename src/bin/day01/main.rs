c85c7d27e9a7199372516288547d29f9013b3627
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-12-04 17:08:28 +0100

    Day 1

---
+++ src/bin/day01/main.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::BufRead;
+
+fn main() {
+    let f = File::open("inputs/day01/input").unwrap();
+    let reader = BufReader::new(f);
+
+    let mut depths: Vec<isize> = vec![];
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        let depth = line.trim().parse::<isize>().unwrap();
+        depths.push(depth);
+    }
+
+    let mut previous: [isize; 3] = [0; 3];
+    let mut increases = 0;
+
+    for (i, depth) in depths.iter().enumerate() {
+        if let 0..=2 = i {
+            previous[i] = *depth;
+            continue;
+        }
+        
+        // TODO: array unpacking?
+        let current: [isize; 3] = [previous[1], previous[2], *depth]; 
+
+        let previous_sum: isize = previous.iter().sum();
+        let current_sum: isize = current.iter().sum();
+        
+        if current_sum > previous_sum {
+            increases += 1;
+        }
+
+        previous.copy_from_slice(&current);
+    }
+
+    println!("{}", increases);
+}
